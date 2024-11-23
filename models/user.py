4dc93583fdea06b59c22c63b3f738339fa360f5b
Omar ACHKIR <you@example.com>
2024-11-23 18:12:11 +0000

    user.py

---
+++ models/user.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" holds class User"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        region = Column(String(128), nullable=True)
+        phone_number = Column(String(128), nullable=True)
+        reviews = relationship("Review", backref="user")
+        bookings = relationship("Booking", backref="user")
+        locations = relationship("Location", backref="user",
+                                 cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        region = ""
+        phone_number = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
