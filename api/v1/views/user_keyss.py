7895ab49fa91c595eaccca15b2dca6a2abb6282c
Omar ACHKIR <you@example.com>
2024-12-05 16:39:04 +0000

    user_keyss.py

--- api/v1/views/user_keyss.py
+++ api/v1/views/user_keyss.py
@@ -8,43 +8,25 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
-@app_views.route('/users/<user_id>/user_keyss', methods=['GET'],
-                 strict_slashes=False)
-@swag_from('documentation/user_keys/user_keyss_by_user.yml', methods=['GET'])
-def get_user_keyss(user_id):
-    """
-    Retrieves the list of all user_keyss objects
-    of a specific User, or a specific user_keys
-    """
-    list_user_keyss = []
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-    for user_keys in user.user_keyss:
-        list_user_keyss.append(user_keys.to_dict())
-
-    return jsonify(list_user_keyss)
-
-
-@app_views.route('/user_keyss/<user_keys_id>/', methods=['GET'], strict_slashes=False)
+@app_views.route('/user_keys/<user_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/user_keys/get_user_keys.yml', methods=['GET'])
-def get_user_keys(user_keys_id):
+def get_user_keys(user_id):
     """
     Retrieves a specific user_keys based on id
     """
-    user_keys = storage.get(User_keys, user_keys_id)
+    user_keys = storage.get(User_keys, user_id)
     if not user_keys:
         abort(404)
     return jsonify(user_keys.to_dict())
 
 
-@app_views.route('/user_keyss/<user_keys_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/user_keys/<user_id>', methods=['DELETE'], strict_slashes=False)
 @swag_from('documentation/user_keys/delete_user_keys.yml', methods=['DELETE'])
-def delete_user_keys(user_keys_id):
+def delete_user_keys(user_id):
     """
     Deletes a user_keys based on id provided
     """
-    user_keys = storage.get(User_keys, user_keys_id)
+    user_keys = storage.get(User_keys, user_id)
 
     if not user_keys:
         abort(404)
@@ -54,7 +36,7 @@ def delete_user_keys(user_keys_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/users/<user_id>/user_keyss', methods=['POST'],
+@app_views.route('/user_keys/<user_id>', methods=['POST'],
                  strict_slashes=False)
 @swag_from('documentation/user_keys/post_user_keys.yml', methods=['POST'])
 def post_user_keys(user_id):
@@ -66,8 +48,12 @@ def post_user_keys(user_id):
         abort(404)
     if not request.get_json():
         abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
+    if 'private_key' not in request.get_json():
+        abort(400, description="Missing private_key")
+    if 'public_key' not in request.get_json():
+        abort(400, description="Missing public_key")
+    if 'shared_key' not in request.get_json():
+        abort(400, description="Missing shared_key")
 
     data = request.get_json()
     instance = User_keys(**data)
@@ -76,13 +62,13 @@ def post_user_keys(user_id):
     return make_response(jsonify(instance.to_dict()), 201)
 
 
-@app_views.route('/user_keyss/<user_keys_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/user_keys/<user_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/user_keys/put_user_keys.yml', methods=['PUT'])
-def put_user_keys(user_keys_id):
+def put_user_keys(user_id):
     """
     Updates a User_keys
     """
-    user_keys = storage.get(User_keys, user_keys_id)
+    user_keys = storage.get(User_keys, user_id)
     if not user_keys:
         abort(404)
 
