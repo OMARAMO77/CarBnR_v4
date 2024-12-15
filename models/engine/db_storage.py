4caef53cd79803919edcfd65a8b6630b3f871a6f
Omar ACHKIR <you@example.com>
2024-12-15 22:54:10 +0000

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -94,6 +94,21 @@ class DBStorage:
 
         return None
 
+    def get_user_by_email(self, cls, email):
+        """
+        Returns the object based on the class name and its Email, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.email == email):
+                return value
+
+        return None
+
     def count(self, cls=None):
         """
         count the number of objects in storage
