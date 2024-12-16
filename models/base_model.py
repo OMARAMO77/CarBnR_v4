c2b3c3ac1b24105bed4aaf8d701a4944e3f23afe
Omar ACHKIR <you@example.com>
2024-12-16 17:33:43 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -45,7 +45,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -71,18 +71,6 @@ class BaseModel:
         if save_fs is None:
             if "password" in new_dict:
                 del new_dict["password"]
-        return new_dict
-
-    def to_dictt(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
         return new_dict
 
     def delete(self):
