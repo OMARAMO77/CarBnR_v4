58d809e11ffac5b24b028e71856efb355a22929b
Omar ACHKIR <you@example.com>
2024-12-16 22:33:18 +0000

    .

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from logging.config import fileConfig
 from models.base_model import Base
 
@@ -6,7 +7,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
