0fb50da84d7225623a038e33f1f30036f18c4519
Omar ACHKIR <you@example.com>
2024-12-18 22:27:18 +0000

    message.py

--- models/message.py
+++ models/message.py
@@ -11,14 +11,13 @@ class Message(BaseModel, Base):
     """Representation of a message"""
     if models.storage_t == 'db':
         __tablename__ = 'messages'
-        sender_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        recipient_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        sender_id = Column(String(36), ForeignKey('users.id', ondelete='SET NULL'),
+                         nullable=True)
+        recipient_id = Column(String(36), ForeignKey('users.id', ondelete='SET NULL'),
+                         nullable=True)
         ciphertext = Column(Text, nullable=False)
         iv = Column(String(128), nullable=False)
         encrypted_key = Column(Text, nullable=False)
-        # ciphertext = Column(LargeBinary, nullable=False)  # Encrypted message content
-        # iv = Column(LargeBinary, nullable=False)  # Initialization vector (16 bytes)
-        # encrypted_key = Column(LargeBinary, nullable=False)  # RSA-encrypted symmetric key
         # Linking relationships
         user_sender = relationship(
             "User",
